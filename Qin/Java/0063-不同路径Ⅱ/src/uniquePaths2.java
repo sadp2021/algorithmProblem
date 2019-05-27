d6c4dff8d7d0f1029693520e7c1804d031637dde
Zhijie Qin <qkuns@qq.com>
2019-05-27 22:32:42 +0800

    63 70

--- Qin/Java/0063-不同路径Ⅱ/src/uniquePaths2.java
+++ Qin/Java/0063-不同路径Ⅱ/src/uniquePaths2.java
@@ -2,6 +2,8 @@ class Solution {
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 		int m = obstacleGrid.length; // row
 		int n = obstacleGrid[0].length; // col
+		if (m == 1 && n == 1 && obstacleGrid[0][0] == 1)
+			return 0;
 		int[][] path = new int[m + 1][n + 1];
 		for (int i = m - 1; i >= 0; i--) {
 			for (int j = n - 1; j >= 0; j--) {
