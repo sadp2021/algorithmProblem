08925a525fd081ba6c6da11b424708508b735cbe
Zhijie Qin <qkuns@qq.com>
2019-05-28 18:27:48 +0800

    746

--- Qin/Java/0070-爬楼梯/src/climbStairs.java
+++ Qin/Java/0070-爬楼梯/src/climbStairs.java
@@ -1,4 +1,24 @@
-
+class Solution {
+    public int climbStairs(int n) {
+    	int a = 1;
+    	int b = 2;
+    	int count = 3;
+        if(n == 1)
+        	return a;
+        else if(n == 2)
+        	return b;
+        else {
+        	while(count <= n) {
+        		switch(count%2) {
+        		case 0:a += b;break;
+        		case 1:b += a;break;
+        		}
+        		count++;
+        	}
+        }
+        return Math.max(a, b);
+    }
+}
 public class climbStairs {
 
 }
