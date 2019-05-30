108a91d9029d36c2294fefcce8e91f75320bad45
Zhijie Qin <qkuns@qq.com>
2019-05-30 23:49:07 +0800

    22 回溯

--- Qin/Java/0022-括号生成/src/generateParenthesis.java
+++ Qin/Java/0022-括号生成/src/generateParenthesis.java
@@ -8,23 +8,21 @@ class Solution {
 	}
 
 	public void backtrack(String str, int rightAble, int left, int count, ArrayList<String> res) {
-		if (rightAble < 0 || left == count)
-			return;
-		backtrack(str + "(", rightAble + 1, left + 1, count, res);
-		backtrack(str + ")", rightAble - 1, left, count, res);
-		if (str.length() == 6) {
+		if (str.length() == 2 * count) {
 			res.add(str);
 			return;
 		}
+		if (left < count)
+			backtrack(str + "(", rightAble + 1, left + 1, count, res);
+		if (rightAble > 0)
+			backtrack(str + ")", rightAble - 1, left, count, res);
 	}
-	
 }
 
 public class generateParenthesis {
 	public static void main(String[] args) {
 		Solution so = new Solution();
-		ArrayList<String> r = so.generateParenthesis(3);
+		ArrayList<String> r = so.generateParenthesis(4);
 		System.out.print(r.toString());
 	}
 }
- 
